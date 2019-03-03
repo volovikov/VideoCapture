87f753140eb4105675bbbb8f99f6f14ed28e2187
v.volovikov <v.volovikov@gmail.com>
2019-03-03 10:49:25 +0300

    fix gitignore + run.js

--- run.js
+++ run.js
@@ -19,7 +19,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password : '',
-    database : 'video-capture'
+    database : 'videocapture'
 });
 connection.connect();
 
